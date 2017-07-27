6f7bcb7824c77c988b013b124a7a6a782cb979e3
minbaba <zhengmin@ypzdw.com>
2017-07-27 16:02:20 +0800

    Initial Commit

---
+++ MBBSlider/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  MBBSlider
+//
+//  Created by 郑敏 on 2017/7/27.
+//  Copyright © 2017年 minbaba. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
